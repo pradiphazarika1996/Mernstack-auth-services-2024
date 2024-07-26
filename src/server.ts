7b8a5a1463dde5b2da5799c9d1226fa585df9e53
pradip <hazarikapradip133@gmail.com>
2024-07-26 09:35:48 +0500

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 // const firstName: string = "pradip";
 // console.log(firstName);
 
-function login(username: string): boolean {
-    console.log(username);
+function login(username: string): string {
+    // console.log(username);
 
-    return true;
+    return username;
 }
 login('dravid');
